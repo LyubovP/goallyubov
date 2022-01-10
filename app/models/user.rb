d7c0ab7ab9f20e0d9284c165a689051e758dc41e
LyubovP <lyubovpidoshva@gmail.com>
2022-01-10 16:37:13 +0000

    add Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable #:confirmable
          
   before_save :avatar
   has_one_attached :avatar
