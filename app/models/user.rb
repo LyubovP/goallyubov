a57f1882d4aece4b4a5be9eecf203d590b960e58
LyubovP <lyubovpidoshva@gmail.com>
2022-01-11 10:42:51 +0000

    add Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable #:confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
          
   before_save :avatar
   has_one_attached :avatar
@@ -13,5 +13,10 @@ class User < ApplicationRecord
   def add_default_avatar
     return if avatar.attached?
       self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.png')), filename: 'default_avatar.png', content_type: 'image/png')
+  end
+  
+  protected
+  def confirmation_required?
+    false
   end
 end
